ed162966d4565a7101919e7180b6137c522023fd
Jan Seipel <janfseipel@gmail.com>
2025-08-10 23:58:23 +0200

    first pwa try

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,38 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Puzzle Profis',
+        short_name: 'PuzzleProfis',
+        description: 'Puzzle game for kids aged 3-5 years',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/puzzle-profis/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/puzzle-profis/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,png,jpg}']
+      }
+    })
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
